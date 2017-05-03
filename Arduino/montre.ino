9bd66dd58132febd527d0c70b227232d3c6787ab
chenmuyao <chenmuyao95@gmail.com>
2017-05-03 22:18:14 +0200

    La montre qui peut afficher le contenu

--- Arduino/montre.ino
+++ Arduino/montre.ino
@@ -38,22 +38,33 @@ SoftwareSerial mySerial(10, 11);
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
-
+// Time message define
 #define TIME_HEADER  "T"    // Header tag for serial time sync message
 #define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
 
+// Info message define
 #define INFO_HEADER "I"    // Header tag for notification message
-int typeInfo;    // Indicator for the type of infomation folowing INFO_HEADER
+int typeInfo = 0;    // Indicator for the type of infomation folowing INFO_HEADER
+char content[20];
 
-const int BUTTON = 7;
+const int BUTTON = 7;    //button define
 
 void setup()  {
+
+  // Initialisation of buzzer
+  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
+
+  // Initialisation of button
+  pinMode(BUTTON, INPUT);
+
   // Initialisation of Bluetooth communication module
   mySerial.begin(38400);    // Set the baud of port to 38400 in order to communicate with Bluetooth
   pinMode(13, OUTPUT);    // Define the indicator Led PIN 13 as output
-  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
+  
+  // Time synchronisation request
   setSyncProvider(requestSync);    // Set function to call when sync required
-  mySerial.println("Waiting for sync message");    // Show the sync require message on the bluetooth terminal
+  mySerial.println("TIME");    // Show the sync require message on the bluetooth terminal
+  
   delay(1000);    // 1 sec delay
   
   // Initialisation of OLED Screen module 
@@ -63,9 +74,11 @@ void setup()  {
   display.setTextColor(WHITE);    // set the text color as white (white seen as blue actually)
   display.setCursor(10,20);    // set the position of the 1st letter
   display.print("WATCH'INT");    // show the welcome page in waiting for the sync
-  tone(buzzer, 2093); // do
-  delay(100);        // ...for 0.1 sec
+  
+  tone(buzzer, 2093); // 0.1 sec of buzz indicating the watch boot
+  delay(100);        
   noTone(buzzer);
+  
   display.display();    // update all the changes to the screen
 
   // if the bluetooth serial communication is established, then process the synchronisation
@@ -73,29 +86,34 @@ void setup()  {
     delay(10);    // wait for the signal 
   }
   processSyncMessage();    // call a function to react to received messages
-  tone(buzzer, 2093); // do
-  delay(100);        // ...for 0.1 sec
-  tone(buzzer, 1568); // Sol
-  delay(100);        // ...for 0,1 sec
+  
+  tone(buzzer, 2093);    // Buzz indicating successful sync 
+  delay(100);
+  tone(buzzer, 1568); 
+  delay(100);
   noTone(buzzer);
 }
 
 /*----------------------------------RESET---------------------------------------------*/
-void (*resetFunc)(void) = 0;
+void (*resetFunc)(void) = 0;    // Set the pointer to the beginning in order to reset
 /*------------------------------------------------------------------------------------*/
 
 void loop(){ 
-  int held = 0;  
+  // Detect the reset signal
+  int held = 0;
   while ((digitalRead(BUTTON) == HIGH) && (held < 10)){
     delay(100);
     held++;
   }
 
   if(held >= 10){
-    resetFunc();
+    resetFunc();    // if the button is held pushed for 1 sec, watch reset
   }
-  checkInfo();
+
+  checkInfo();    // Notification checking
+  delay(800);
   timeshow();    // Time showing
+
 }
 
 /*-------------------------------------AFFICHAGE DU TEMPS ET DE L'HEURE-------------------------------------------------------*/
@@ -112,8 +130,8 @@ void timeshow(){
   } else {
     digitalWrite(13, LOW);  // LED off if needs refresh
   }
-  //delay(1000);
 }
+
 void digitalClockDisplay(){
   // digital clock display of the time
   display.setTextSize(2);
@@ -161,30 +179,77 @@ time_t requestSync()
 /*------------------------------------------------------------------------------------------------------------------------------*/
 
 /*-----------------------------------------CHECK INFO--------------------------------------------------------------------*/
-void checkInfo(){
+void checkInfo() {
   /* Check for new notification*/
+  int typeInfo_before = typeInfo;
+  
   if(mySerial.available()) {
     if (mySerial.find(INFO_HEADER))
     {
       typeInfo = mySerial.parseInt();    // extract type of information
     }
   }
-  /* We define 0 as notifications read*/
-  if (typeInfo != 0){
-    showInfo(typeInfo);    //if there is new notification
-  }
-  else display.clearDisplay();
+      
+    
+    int typeInfo_local = typeInfo;
+    int Nb_S;
+    int Nb_M;
+    int Nb_R;
+
+    Nb_R = typeInfo_local%10;
+    typeInfo_local = typeInfo_local/10;
+    Nb_M = typeInfo_local%10;
+    typeInfo_local = typeInfo_local/10;
+    Nb_S = typeInfo_local%10;
+    showNotif(Nb_S, Nb_M, Nb_R);
+
+    if (typeInfo_before != typeInfo)
+    {
+
+    if ((Nb_S != 0) || (Nb_M != 0) || (Nb_R != 0))
+    {
+      for (int i = 0; i < 20 ; ++i)
+      {
+        content[i] = mySerial.read();
+        delay(10);
+      }
+
+    } 
+    else 
+    {
+      for (int i = 0; i < 20 ; ++i)
+        {
+          content[i] = '\0';
+        }
+    }  
+  } 
+    showContent(content);
+  
+  
 }
 
-void showInfo(int typeInfo) {
+void showNotif(int Nb_S, int Nb_M, int Nb_R) {
   display.setTextSize(1);
-  display.setTextColor(WHITE);    //Set the cursor 
-  display.setCursor(5,5);
-  switch (typeInfo) {    // judge the type of notificaiton
-    case 1 : display.print("Vous avez un nouveau SMS!"); break;
-    case 2 : display.print("Vous avez un nouveau mail!"); break;
-    case 3 : display.print("Vous avez un rappel!"); break;
-    default : display.print("WTF?!");
+  display.setTextColor(WHITE);
+  display.setCursor(1,1);
+  display.print("S : ");
+  display.print(Nb_S);
+  display.print("  ");
+  display.print("M : ");
+  display.print(Nb_M);
+  display.print("  ");
+  display.print("R : ");
+  display.print(Nb_R);
+  display.display();
+}
+
+void showContent(char content[]) {
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(10,10);
+  for (int i = 0; i < 20; ++i)
+  {
+    display.print(content[i]);
   }
   display.display();
 }
